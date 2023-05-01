3e4b467ab52272129e22308fbaf8f267b9f4fb04
absolutelydawn <absolutelydawnn@gmail.com>
2023-05-01 15:27:35 +0900

    commit test

---
+++ Assets/Hello.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("WasSheSlow?");
+        Debug.Log("NO");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
